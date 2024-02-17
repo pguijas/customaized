7b07dd56ea7865f2398bf6924011ecde3fe34e28
Elena Sanchez Gonzalez <elena.sanchezg@udc.es>
2024-02-17 11:46:32 +0100

    feature: added user interface

---
+++ ui/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"wBJ0u+vNarCrMJo+mch05P+wvYYbgAjpPizguK/EB7Y=\"\n}"
---
+++ ui/app/login/page.tsx
@@ -0,0 +1,46 @@
+export default async function Login() {
+  return (
+    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
+      <form
+        className="flex-1 flex flex-col w-full justify-center gap-2 "
+        action="/auth/sign-in"
+        method="post"
+      >
+        <Card>
+          <CardHeader>
+            <CardTitle>Log In / Sign Up</CardTitle>
+            <CardDescription>
+              Log into your account or sign up for a new one to get started.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <Label className="text-md" htmlFor="email">
+              Email
+            </Label>
+            <input
+              className="rounded-md px-4 py-2 bg-inherit border"
+              name="email"
+              placeholder="you@example.com"
+              required
+            />
+            <Button>Continue</Button>
+            <Messages />
+          </CardContent>
+          <CardFooter>
+            <p className="text-sm">
+              By signing up, you agree to our{" "}
+              <a href="#" className="underline">
+                Terms of Service
+              </a>{" "}
+              and{" "}
+              <a href="#" className="underline">
+                Privacy Policy
+              </a>
+              .
+            </p>
+          </CardFooter>
+        </Card>
+      </form>
+    </div>
+  );
+}
