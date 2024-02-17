083bf7a046ad91f170ec1d8cb4f9f7fa63d07c45
Elena Sanchez Gonzalez <elena.sanchezg@udc.es>
2024-02-17 13:30:28 +0100

    feature: added ui for login

---
+++ ui/src/app/login/page.tsx
@@ -0,0 +1,44 @@
+export default async function Login() {
+    return (
+        <div className="w-full lg:max-w-xl p-6 space-y-8 sm:p-8 bg-white rounded-lg shadow-xl bg-gray-150 items-center">
+            <h2 className="text-2xl font-bold text-gray-900">
+                Sign in to CustomAIzed
+            </h2>
+            <form className="mt-8 space-y-6" action="#">
+                <div>
+                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your
+                        email</label>
+                    <input type="email" name="email" id="email"
+                           className="border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-100 border-gray-600 placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                           placeholder="name@company.com" required/>
+                </div>
+                <div>
+                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Your
+                        password</label>
+                    <input type="password" name="password" id="password" placeholder="••••••••"
+                           className="border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-100 border-gray-600 placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                           required/>
+                </div>
+                <div className="flex items-start">
+                    <div className="flex items-center h-5">
+                        <input id="remember" aria-describedby="remember" name="remember" type="checkbox"
+                               className="w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600"
+                               required/>
+                    </div>
+                    <div className="ms-3 text-sm">
+                        <label htmlFor="remember" className="font-medium text-gray-500 dark:text-gray-400">Remember this
+                            device</label>
+                    </div>
+                </div>
+                <div className="container py-5 px-5 mx-0 min-w-full flex flex-col items-center">
+                    <button type="button"
+                            className="text-white bg-gradient-to-r bg-blue-500 hover:bg-gradient-to-br font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                        Login
+                    </button>
+                </div>
+            </form>
+        </div>
+    )
+        ;
+
+}
--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -45,19 +45,19 @@ export default function Home() {
           bringing you a unique and personalized experience like never before.
         </p>
         <div>
-          <Link href="/login">
-            <button className="bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded">
-              Get Started
-            </button>
-          </Link>
-          <div className="mt-4 text-gray-500">
-            <span>Already a member? </span>
-            <Link className="text-blue-600 hover:underline" href="/login">
-              Sign In
+            <Link href="/login">
+                <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                    Get Started
+                </button>
             </Link>
-          </div>
+            <div className="mt-4 text-gray-500">
+                <span>Already a member? </span>
+                <Link className="text-blue-600 hover:underline" href="/login">
+                    Sign In
+                </Link>
+            </div>
         </div>
-        <hr className="my-9 border-gray-200" />
+          <hr className="my-9 border-gray-200" />
         <Howto />
       </div>
   );
